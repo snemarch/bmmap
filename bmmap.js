be5e87726c7f64809c59ba776adccf4e545c4f86
Sune Marcher <sm@flork.dk>
2018-12-27 13:34:34 +0100

    Convert spaces to tabs

--- bmmap.js
+++ bmmap.js
@@ -2,138 +2,137 @@
 "use strict";
 
 var state = {
-    usersById: null,
-    usersByName: null,
-    graphContainer: null,
-    graph: null
+	usersById: null,
+	usersByName: null,
+	graphContainer: null,
+	graph: null
 };
 
 fetch("data/bmmap.json")
 .then(function(response) {
-    return response.json();
+	return response.json();
 }).then(function(json) {
-    doParse(json);
-    state.graphContainer = document.getElementById('graph');
-
+	doParse(json);
+	state.graphContainer = document.getElementById('graph');
 });
 
 function addContact(users, a, b) {
-    var user = users[a];
-    if(!user.contacts) {
-        user.contacts = [];
-    }
+	var user = users[a];
+	if(!user.contacts) {
+		user.contacts = [];
+	}
 
-    var friend = users[b];
-    user.contacts.push(friend);
+	var friend = users[b];
+	user.contacts.push(friend);
 }
 
 function buildGraph(users, edges) {
-    edges.forEach(e => {
-        addContact(users, e.A, e.B);
-        addContact(users, e.B, e.A);
-    });
+	edges.forEach(e => {
+		addContact(users, e.A, e.B);
+		addContact(users, e.B, e.A);
+	});
 }
 
 function subselect(output, node, visited, depth) {
-    if(depth < 1) {
-        return;
-    }
+	if(depth < 1) {
+		return;
+	}
 
-    if(node.isProcessed) {
-        console.log(`User ${node.userName} already processed`);
-        return;
-    }
-    node.isProcessed = true;
+	if(node.isProcessed) {
+		console.log(`User ${node.userName} already processed`);
+		return;
+	}
+	node.isProcessed = true;
 
-    node.contacts.forEach(c => {
-        output.edges.push({from: node.userId, to: c.userId });
-        if(!visited[c.userId]) {
-            output.nodes.push(c);
+	node.contacts.forEach(c => {
+		output.edges.push({from: node.userId, to: c.userId });
+		if(!visited[c.userId]) {
+			output.nodes.push(c);
 
-            subselect(output, c, visited, depth - 1);
-            visited[c.userId] = true;
-        }
-    });
+			subselect(output, c, visited, depth - 1);
+			visited[c.userId] = true;
+		}
+	});
 }
 
 function doParse(json) {
-    console.log("Making user lookup maps...");
-    state.usersById = _.keyBy(json.users, 'userId');
-    state.usersByName = _.keyBy(json.users, 'userName');
+	console.log("Making user lookup maps...");
+	state.usersById = _.keyBy(json.users, 'userId');
+	state.usersByName = _.keyBy(json.users, 'userName');
 
-    console.log("Building graph...");
-    buildGraph(state.usersById, json.edges);
+	console.log("Building graph...");
+	buildGraph(state.usersById, json.edges);
 
-    document.getElementById('renderButton').disabled = false;
+	document.getElementById('renderButton').disabled = false;
 }
 
 function clickRender() {
-    var userName = document.getElementById('userName');
-    var depth = document.getElementById('depth');
+	var userName = document.getElementById('userName');
+	var depth = document.getElementById('depth');
 
-    depth = parseInt(depth.options[depth.selectedIndex].value, 10);
+	depth = parseInt(depth.options[depth.selectedIndex].value, 10);
 
-    renderUser(userName.value, depth);
+	renderUser(userName.value, depth);
 }
 
 function renderUser(userName, depth) {
-    console.log("Building subtree...");
-    var root = state.usersByName[userName];
-    if(!root) {
-        alert("User not found!");
-        return;
-    }
-    if(root.isProcessed) {
-        console.log(`User ${userName} already processed`);
-    }
+	console.log("Building subtree...");
+	var root = state.usersByName[userName];
+	if(!root) {
+		alert("User not found!");
+		return;
+	}
+	if(root.isProcessed) {
+		console.log(`User ${userName} already processed`);
+	}
 
-    var subtree = {nodes:[root], edges:[]};
-    var visited = [];
-    visited[root.userId] = true;
+	var subtree = {nodes:[root], edges:[]};
+	var visited = [];
+	visited[root.userId] = true;
 
-    subselect(subtree, root, visited, depth);
-    
-    try {
-        initializeGraph(subtree);
-    }
-    catch(ex) {
-        console.error(ex);
-        return;
-    }
+	subselect(subtree, root, visited, depth);
+	
+	try {
+		initializeGraph(subtree);
+	}
+	catch(ex) {
+		console.error(ex);
+		return;
+	}
 }
 
 function initializeGraph(subtree) {
-    if(state.graph != null) {
-        return;
-    }
+	if(state.graph != null) {
+		return;
+	}
 
-    console.log("Constructing VisJS stuff...");
-    var nodes = new vis.DataSet(_.map(subtree.nodes, o => {
-            return {
-                id: o.userId,
-                label: o.userName
-            };
-        }));
+	console.log("Constructing VisJS stuff...");
+	var nodes = new vis.DataSet(_.map(subtree.nodes, o => {
+			return {
+				id: o.userId,
+				label: o.userName
+			};
+		}));
 
-    var edges = new vis.DataSet(subtree.edges);
+	var edges = new vis.DataSet(subtree.edges);
 
-    var visData = { nodes: nodes, edges: edges };
-    var options = {
-        physics: {
-            enabled: true,
-            // barnesHut: {
-            //     avoidOverlap: 0.5
-            // }
-        },
-        layout: {
-            // hierarchical: true,
-            improvedLayout: false
-        }
-    };
+	var visData = { nodes: nodes, edges: edges };
+	var options = {
+		physics: {
+			enabled: true,
+			// barnesHut: {
+			//     avoidOverlap: 0.5
+			// }
+		},
+		layout: {
+			// hierarchical: true,
+			improvedLayout: false
+		}
+	};
 
-    state.graph = new vis.Network(state.graphContainer, visData, options);
-    state.graph.on("selectNode", function(node) {
-        console.log("Selected: ", state.usersById[node.nodes[0]].userName);
-    });
-    console.log("Done!");
+	state.graph = new vis.Network(state.graphContainer, visData, options);
+	state.graph.on("selectNode", function(node) {
+		console.log("Selected: ", state.usersById[node.nodes[0]].userName);
+	});
+	console.log("Done!");
 }
